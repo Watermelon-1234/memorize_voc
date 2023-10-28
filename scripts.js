ad9f7e571f1bc3032b18a3b9ae40ad4c9ffc5f58
Watermelon-1234 <948794harvey@gmail.com>
2023-10-29 00:58:28 +0800

    highlighter

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ function pickWords() {
     var numToPick = parseInt(document.getElementById("numToPick").value);
     var outputDiv = document.getElementById("outputDiv");
     //console.log(numToPick);
-    if (startLetter !== '' && endLetter !== '' && numToPick ) {
+    if (startLetter !== '' && endLetter !== '' && numToPick >0) {
         outputDiv.innerHTML = "<h2>請等等...</h2><ul>";
 
         pickAndOutputWords(startLetter, endLetter, numToPick)
@@ -87,18 +87,32 @@ function displayWords(words) {
     //console.log(words);
     var content = "<h2>抽到的單字：</h2><table>";
     for (var i = 0; i < words.length; i++) {
-        content += "<tr><td><input type=\"button\" onclick=\"high_light(this) \" value=\"我不會\"></td><td>" + words[i][0] + "</td><td><input type=\"button\" onclick=\"showChinese(this,\'"+ words[i][1] + "\' ) \" value=\"顯示中文\"></td></tr>";
+        content += "<tr><td>" + words[i][0] + "</td><td><input type=\"button\" onclick=\"high_light(this) \" value=\"我不會\"></td><td><input type=\"button\" onclick=\"showChinese(this,\'"+ words[i][1] + "\' ) \" value=\"顯示中文\"></td></tr>";
     }
     content += "</table>";
+    content += "<input type=\"button\" onclick=\"toggleRowVisibility(this)\" value=\"只出現不會的字\"></input>";
     outputDiv.innerHTML = content;
 }
 function high_light(button) {
-  var row = button.parentNode.parentNode; 
+
+  var row = button.parentNode.parentNode;
   var cells = row.getElementsByTagName('td');
-  
-  for(var i = 0; i < cells.length; i++) {
-    cells[i].style.color =  '#FF00FF'; 
+  var currentColor = window.getComputedStyle(cells[1]).color; 
+  if(currentColor == 'rgb(255, 0, 255)') {
+    // 取消高亮
+    for(var i = 0; i < cells.length; i++) {
+      cells[i].style.color = '#00FF00';
+    }
+    button.value = "我不會";
+  } else {  
+    // 设置高亮
+    button.value = "我會了";
+    for(var i = 0; i < cells.length; i++) {
+      cells[i].style.color = '#FF00FF';
+    }
   }
+
+
 }
 
 // Usage:
@@ -120,3 +134,63 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("startLetter").addEventListener("change", populateEndLetters);
     document.getElementById("startLetter").dispatchEvent(new Event("change"));
 });
+
+
+// 高亮颜色
+var highlightColor = '#FF00FF';
+
+// 高亮行变量
+var highlightRow=false;  
+function toggleRowVisibility(button) {
+  
+  // 获取表格所有行
+  //var table = document.getElementsByTagName('table');
+  var rows = document.getElementsByTagName('tr');
+  // 检查是否有隐藏行
+  var hasHiddenRows = false;
+  var rowStyle
+  for(var i = 0; i < rows.length; i++) {
+    if(rows[i].style.display == 'none') {
+      hasHiddenRows = true;
+      break;
+    }
+  }
+
+  // 切换高亮行显示状态
+  if(hasHiddenRows) {//復原
+    highlightRow = false;
+    button.value = "只出現不會的字";
+    // 遍历行
+      for(i = 0; i < rows.length; i++) {
+    
+        // 获取行的样式
+        rowStyle = window.getComputedStyle(rows[i]).display;
+
+        if(rowStyle == 'none')//隱藏
+        {
+          rows[i].style.display = '';//復原
+        } 
+      }
+  } 
+  else {//隱藏
+    hasHiddenRows = true;
+    button.value = "顯示所有單字";
+    // 遍历行
+      for(i = 0; i < rows.length; i++) {
+        ele=rows[i].getElementsByTagName('td');
+        // 获取行的样式
+        rowStyle = window.getComputedStyle(ele[1]).color;
+        
+        console.log(rowStyle);
+        
+        
+        if(rowStyle == 'rgb(255, 0, 255)')//已變色
+        {
+          //rows[i].style.display = '';//不用理他
+        } 
+        else {
+          rows[i].style.display = 'none'; 
+        }
+      }
+  }
+}
